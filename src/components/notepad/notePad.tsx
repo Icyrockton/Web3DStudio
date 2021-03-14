e27f014ab4b6fd434eaaa9c8b062bb3545991399
Icyrockton <42564541+Icyrockton@users.noreply.github.com>
2021-03-14 10:50:15 +0800

    笔记漂浮功能

--- src/components/notepad/notePad.tsx
+++ src/components/notepad/notePad.tsx
@@ -3,13 +3,21 @@ import React, {useEffect, useState} from 'react';
 import 'react-markdown-editor-lite/lib/index.css';
 import classes from "./notePad.module.css"
 import Layout, {Content, Header} from "antd/es/layout/layout";
-import {Button, Col, Input, Menu, Row, Space} from "antd";
+import {Button, Col, Input, Menu, Row, Space, Tooltip} from "antd";
 import {createLocalStorageStateHook} from "use-local-storage-state";
 import Sider from "antd/es/layout/Sider";
 import 'antd/dist/antd.css';
 import MdEditor from 'react-markdown-editor-lite'
-import {DeleteFilled, HighlightOutlined, PlusCircleFilled, PlusCircleTwoTone} from "@ant-design/icons";
+import {
+    CloseCircleFilled, CloseOutlined, CloseSquareFilled,
+    DeleteFilled,
+    FormOutlined,
+    HighlightOutlined,
+    PlusCircleFilled,
+    PlusCircleTwoTone
+} from "@ant-design/icons";
 import Paragraph from "antd/es/typography/Paragraph";
+import {Rnd} from "react-rnd";
 
 
 const markDownParser = new MarkdownIt(); //markdown解析器
@@ -54,6 +62,8 @@ const NotePad = (props: NotePadProps) => {
 
     const [tabKey, setTabKey] = useState<string>(defaultKey);
     const [collapse, setCollapse] = useState(true);
+    const [xy, setXy] = useState({x: 80, y: 400});
+    const [size, setSize] = useState({width: "200px", height: "300px"});
     const notePadItem = () => {
         return notePads.map((notePad, index) =>
             <Menu.Item key={notePad.key}>{notePad.title}</Menu.Item>
@@ -118,24 +128,30 @@ const NotePad = (props: NotePadProps) => {
             <>
                 <Header className={classes.header}>
                     <Row justify={"center"} align="middle">
-                        <Col span={20}>
+                        <Col span={18} className={"no-drag"}>
                             <Input value={notePad.title} className={classes.editTitle}
                                    onChange={event => updateNotePadTitle(event.target.value)}/>
                         </Col>
 
                         <Col span={4}>
                             <Button type="primary" shape="round" danger={true} icon={<DeleteFilled/>} size={"large"}
-                                 disabled={notePads.length == 1}     className={classes.centerButton} onClick={deleteNotePad}>
+                                    disabled={notePads.length == 1} className={classes.centerButton}
+                                    onClick={deleteNotePad}>
                                 删除
                             </Button>
                         </Col>
-
+                        <Col span={2}>
+                            <Button type="default" icon={<CloseOutlined/>} size={"large"}
+                                    className={classes.centerButton} onClick={() => setShowing(false)}
+                            >
+                            </Button>
+                        </Col>
 
                     </Row>
                 </Header>
                 <Content style={{
                     padding: "24px"
-                }}> {editor}
+                }} className={"no-drag"}> {editor}
                 </Content>
             </>
         )
@@ -149,16 +165,41 @@ const NotePad = (props: NotePadProps) => {
     }
     return (
         <>
-            <div className={`${classes.notePad} ${isShowing ? classes.notePadSlide : ""}`}>
-                <div className={classes.slideButton} onClick={() => setShowing(!isShowing)}>
-                    <h2 className={classes.slideButtonText}>
-                        {hint()}
-                    </h2>
+            <div className={classes.openButtonArea}>
+                <Tooltip title={hint()}>
+                    <Button icon={<FormOutlined style={{fontSize: "30px"}}/>} shape={"circle"}
+                            onClick={()=>setShowing(!isShowing)}
+                            className={classes.openButton}/>
+                </Tooltip>
+            </div>
+
+
+            <Rnd size={{width: size.width, height: size.height}}
+                 className={` ${isShowing ? "":classes.none}`}
+                 position={{x: xy.x, y: xy.y}}
+                 onDragStop={(e, data) => setXy({x: data.x, y: data.y})}
+                 onResizeStop={(e, direction, ref, delta, position) => {
+                     setSize({
+                         width: ref.style.width,
+                         height: ref.style.height,
+                     });
+                 }}
+                 minWidth={"40%"}
+                 minHeight={"40%"}
+                 maxWidth={"80%"}
+                 maxHeight={"80%"}
+                 cancel={".no-drag"}
+                 enableResizing={
+                     {bottom:true , right:true , bottomRight:true}
+                 }
+            >
+                <div className={classes.dragArea} >
+
                 </div>
-                <div className={classes.notePadEditor}>
-                    <Layout style={{height: "100%"}}>
+                <div className={`${classes.notePadEditor} no-drag`}>
+                    <Layout style={{height: "100%"}} >
                         {/*侧边栏*/}
-                        <Sider className={classes.notePadSider} theme={"light"}>
+                        <Sider className={`${classes.notePadSider} `} theme={"light"}>
                             <div className={classes.newNote}>
                                 <Button type="primary" shape="round" icon={<PlusCircleFilled/>} size={"large"}
                                         onClick={addNewNotePad}>
@@ -178,8 +219,7 @@ const NotePad = (props: NotePadProps) => {
                         </Layout>
                     </Layout>
                 </div>
-
-            </div>
+            </Rnd>
         </>
     )
 }
